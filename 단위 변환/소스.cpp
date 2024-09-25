9f6b68e0517c8f5ff5df61ad702d0eb89218c8c2
한상현 <sang1121qwerqwet@gmail.com>
2024-09-25 18:57:08 +0900

    단위변환 수정

--- 단위 변환/소스.cpp
+++ 단위 변환/소스.cpp
@@ -2,14 +2,19 @@
 
 int main(void)
 {
-	double n;
+	double n1;
+	double n2;
 
-	printf("숫자 입력 : ");
-	scanf_s("%lf", &n);
+	printf("숫자 입력 (ft, in 또는 slug), (m 또는 kg) : ");
+	scanf_s("%lf%lf", &n1, &n2);
 
-	printf("%lfft = %lfm\n", n, 0.3048 * n);
-	printf("%lfin = %lfm\n", n, 0.0254 * n);
-	printf("%lfkg = %lfslug\n", n, 0.0685218 * n);
+	printf("%.1lfft = %lfm\n", n1, 0.3048 * n1);
+	printf("%.1lfin = %lfm\n", n1, 0.0254 * n1);
+	printf("%.1lfslug = %lfkg\n", n1, 14.5939 * n1);
+	printf("%.1lfm = %lfft\n", n2, 3.28084 * n2);
+	printf("%.1lfm = %lfin\n", n2, 39.3701 * n2);
+	printf("%.1lfkg = %lfslug\n", n2, 0.0685218 * n2);
+
 
 	return 0;
 }
