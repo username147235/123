93830ec1377db1eb0ff0d8189236ca6a340699bf
한상현 <sang1121qwerqwet@gmail.com>
2024-12-16 15:09:22 +0900

    kljkjljlk

--- 문자열 뒤집기/소스.cpp
+++ 문자열 뒤집기/소스.cpp
@@ -6,19 +6,18 @@
 int main(void)
 {
 	char str[l];
-	char* p = str;
 	char c[50];
 
 	printf("문자열 입력: ");
 	fgets(str, sizeof(str), stdin);
 
-	printf("입력받은 문자열: %s", str);
-	str[strcspn(str, " ")] = ' ';
+	str[strcspn(str, "\n")] = ' ';
 
-	int a, j, k = 0;
+	int a, j, k, i = 0;
 	char w[50];
 	char rev_w[50];
-	for (int i = 0; i < 100; i + 2)
+	printf("\n변형된 문자열: ");
+	for (i = 0; i < 1000; i++)
 	{
 		for (j = 0; j < 50; j++)
 		{
@@ -28,7 +27,9 @@ int main(void)
 				for (a = j-1; a >= 0; a--) 
 				{
 					printf("%c", w[a]);
+					w[a] = '\0';
 				}
+				printf(" ");
 				i += j;
 				j = 50;
 			}
